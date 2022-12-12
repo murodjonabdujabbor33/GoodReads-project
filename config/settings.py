f2ccbdb213340e57d4af2506b8bc06bae2955cf8
Murodjon Abdujabbor <murodjonabdujabbor33@gmail.com>
2022-12-12 23:27:55 +0500

    updated

--- config/settings.py
+++ config/settings.py
@@ -66,8 +66,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'goodreads',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'USER': 'postgres',
+        'PASSWORD': 'Murodjon8',
     }
 }
 
