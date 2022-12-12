6249d70599bc1bda26397f3a552311c93dc78c00
Murodjon Abdujabbor <murodjonabdujabbor33@gmail.com>
2022-12-12 22:36:40 +0500

    add Author model

--- books/models.py
+++ books/models.py
@@ -6,4 +6,9 @@ class Book(models.Model):
     title = models.CharField(max_length=200)  # VARCHAR()
     description = models.TextField()
     isbn = models.CharField(max_length=17)
-    
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    bio = models.TextField()
