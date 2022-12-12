742936aa678503e183bfc14f2a0036fadbce5693
Murodjon Abdujabbor <murodjonabdujabbor33@gmail.com>
2022-12-12 22:32:37 +0500

    add Book model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)  # VARCHAR()
+    description = models.TextField()
+    isbn = models.CharField(max_length=17)
+    
