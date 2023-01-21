ab5323bc26aa16cdc64e34a3809b040d1ded3c72
murodjonabdujabbor33 <murodjonabdujabbor33>
2023-01-21 10:36:09 +0500

    Add login and register page

--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class RegisterView(View):
+    def get(self, request):
+        return render(request, "users/register.html")
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, "users/login.html")
