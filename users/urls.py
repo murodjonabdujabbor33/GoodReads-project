ea7ed3920425c2c9a492b7c9779ed51847d3b75f
murodjonabdujabbor33 <murodjonabdujabbor33>
2023-01-21 12:58:44 +0500

    add registeration form

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from users.views import RegisterView, LoginView
 
-
+app_name = "users"
 urlpatterns = [
     path("register", RegisterView.as_view(), name="register"),
     path("login", LoginView.as_view(), name="login"),
